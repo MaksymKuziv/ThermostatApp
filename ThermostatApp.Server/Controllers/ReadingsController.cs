8b0588530962bfb18f3cc16d77ad6878b053adc5
Максим Кузив <maksim_kyziv@outlook.com>
2025-10-27 21:01:25 -0400

    Add POST with DTO and GET with take parameter to ReadingsController

--- ThermostatApp.Server/Controllers/ReadingsController.cs
+++ ThermostatApp.Server/Controllers/ReadingsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ThermostatApp.Server.Data;
 using ThermostatApp.Server.Models;
+using ThermostatApp.Server.Dtos;
 
 namespace ThermostatApp.Server.Controllers
 {
@@ -18,18 +19,29 @@ namespace ThermostatApp.Server.Controllers
 
         // GET: api/readings
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Reading>>> GetReadings()
+        public async Task<ActionResult<IEnumerable<Reading>>> GetReadings([FromQuery] int take = 20)
         {
             return await _context.Readings
                 .OrderByDescending(r => r.CreatedAtUtc)
+                .Take(take)
                 .ToListAsync();
         }
 
         // POST: api/readings
         [HttpPost]
-        public async Task<ActionResult<Reading>> PostReading(Reading reading)
+        public async Task<ActionResult<Reading>> PostReading([FromBody] ReadingCreateDto dto)
         {
-            reading.CreatedAtUtc = DateTime.UtcNow;
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reading = new Reading
+            {
+                TemperatureC = dto.TemperatureC!.Value,
+                Location = dto.Location,
+                Notes = dto.Notes,
+                CreatedAtUtc = DateTime.UtcNow
+            };
+
             _context.Readings.Add(reading);
             await _context.SaveChangesAsync();
 
---
+++ ThermostatApp.Server/Dtos/ReadingCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThermostatApp.Server.Dtos
+{
+    public class ReadingCreateDto
+    {
+        [Required]
+        public double? TemperatureC { get; set; }
+
+        [MaxLength(100)]
+        public string? Location { get; set; }
+
+        [MaxLength(500)]
+        public string? Notes { get; set; }
+    }
+}
